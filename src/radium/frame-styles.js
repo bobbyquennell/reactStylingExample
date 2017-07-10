4c86639b92223793c96af20eb08b6b98b365e5d9
bobbyquennell <bobbyquennell@gmail.com>
2017-07-10 22:47:43 +1000

    sytling component: frame(wrapped with Radium component)

---
+++ src/radium/frame-styles.js
@@ -0,0 +1,10 @@
+import config from './config-styles'
+
+export default {
+    root:{
+        position:'relative',
+        maxWidth:'100%',
+        width: config.ImageWidth,
+        margin:'50px auto'
+    }
+}
