6248af5466a77206122e085dead91baf72aedf67
bobbyquennell <bobbyquennell@gmail.com>
2017-07-10 11:48:13 +1000

    restucture project files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import DriftApp from './app'
+import DriftApp from './inline/app'
 
 ReactDOM.render(<DriftApp />, document.getElementById('app'))
