fbc7f06d10f3e680a2ca187da14c6e32b86bbcd8
bobbyquennell <bobbyquennell@gmail.com>
2017-07-11 17:12:30 +1000

    styling component:nav(wrapped with Radium)

---
+++ src/radium/nav-styles.js
@@ -0,0 +1,47 @@
+import config from './config-styles'
+
+const btn ={
+    flex:1,
+    height:config.imageHeight,
+    padding:20,
+    verticalAlign:'top',
+    fontSize:50,
+    background:'transparent',
+    color:'#fff',
+    textShadow: '1px 1px 3px rgba(0, 0, 0, .3)',
+    border:'none',
+    outline:0,
+    cursor: 'pointer',
+    useSelect:'none'
+
+}
+const hidden = {
+    visibility: 'hidden'
+}
+const prev ={
+  ...btn,
+  textAlign:'left'
+}
+const next = {
+  ...btn,
+  textAlign:'right'
+}
+export default {
+    root:{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        display:'flex'
+    },
+    prev,
+    next,
+    prevHidden:{
+        ...prev,
+        ...hidden
+    },
+    nextHidden:{
+        ...next,
+        ...hidden
+    }
+}
