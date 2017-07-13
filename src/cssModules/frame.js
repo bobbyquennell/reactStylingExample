464e9e5b80a5f05171d51d07b2320c006bb05ca8
bobbyquennell <bobbyquennell@gmail.com>
2017-07-13 14:24:11 +1000

    styling component: frame in a CSS Module way

--- src/cssModules/frame.js
+++ src/cssModules/frame.js
@@ -1,5 +1,18 @@
 import React from 'react'
+//make our css modules export object available in our component via
+//react-styleable
+import styleable from 'react-styleable'
+import css from './styles/frame.css'
 
-export default function Frame({ children }) {
-  return <div>{children}</div>
+// export default function Frame({ children }) {
+//   return <div>{children}</div>
+// }
+
+
+//wrapp our component in react-styleable higher order component that receives
+//our CSS module and passes it on as props.css to our component
+//thus the root class is available as props.css.root.
+function Frame(props) {
+  return <div className={props.css.root}>{props.children}</div>
 }
+export default styleable(css)(Frame)
