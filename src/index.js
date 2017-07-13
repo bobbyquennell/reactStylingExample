8d8852fd680386f01809d0275b4bbea40c8184d4
bobbyquennell <bobbyquennell@gmail.com>
2017-07-13 11:15:11 +1000

    initial setup for a CSS Module demo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import DriftApp from './cssStylingWithWebpack/app'
+import DriftApp from './cssModules/app'
 
 ReactDOM.render(<DriftApp />, document.getElementById('app'))
