db110c0ea603f9e7fcab5dfd46d35e6768125db3
bobbyquennell <bobbyquennell@gmail.com>
2017-07-13 15:23:25 +1000

    enchancement: disable prev/next btn when no images available

--- src/cssModules/nav.js
+++ src/cssModules/nav.js
@@ -3,11 +3,20 @@ import styleable from 'react-styleable'
 import css from './styles/nav.css'
 const { func, bool } = React.PropTypes
 
+
+function getPrevClassName(props){
+    return props.hasPrevious ? props.css.prev : props.css.prevHidden;
+}
+
+function getNextClassName(props){
+    return props.hasNext ? props.css.next : props.css.nextHidden;
+}
+
 function Nav(props) {
   return (
     <div className={props.css.root}>
-      <button onClick={props.onPrevious} className={props.css.prev}>&#10094;</button>
-      <button onClick={props.onNext} className={props.css.next}>&#10095;</button>
+      <button onClick={props.onPrevious} className={getPrevClassName(props)}>&#10094;</button>
+      <button onClick={props.onNext} className={getNextClassName(props)}>&#10095;</button>
     </div>
   )
 }
